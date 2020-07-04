a7a5ab544741dc4e450d60c8c48b151716123dda
Francis Masha <francismasha96@gmail.com>
2020-07-04 04:05:51 +0300

    feat: add device output pin class

---
+++ src/DevicePinOutput.cpp
@@ -0,0 +1,30 @@
+//Includes---------------------------------------------------------------------
+#include "AlmondPrecompiled.h"
+#include "DevicePinOutput.h"
+
+
+//Implementation---------------------------------------------------------------
+DevicePinOutput::DevicePinOutput(const char *name, uint8_t pin, bool invert)
+	:
+	DeviceOutput(name)
+{
+	this->pin = pin;
+	this->invert = invert;
+}
+
+void DevicePinOutput::setup()
+{
+	pinMode(this->pin, OUTPUT);
+	set_value(this->value);
+}
+
+void DevicePinOutput::set_value(int value, bool verbose)
+{
+	bool write_value = !invert ? (value != 0) : (value == 0);
+	digitalWrite(this->pin, write_value);
+
+	if (this->value != value && verbose) {
+		printf("Device '%s' status now: %d", this->name, value);
+	}
+	this->value = value;
+}
---
+++ src/DevicePinOutput.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+//Includes---------------------------------------------------------------------
+#include "AlmondPrecompiled.h"
+#include "Device.h"
+
+
+//Types------------------------------------------------------------------------
+class DevicePinOutput: public DeviceOutput
+{
+public:
+	DevicePinOutput(const char *name, uint8_t pin, bool invert);
+	void setup() override;
+	void set_value(int value, bool verbose = false) override;
+
+protected:
+	uint8_t pin;
+	bool invert;
+};
